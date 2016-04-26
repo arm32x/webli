677f493b662a91e4b001f95675fb629a22acba8c
cummconn053 <cummconn053@gotvdsb.ca>
2016-04-26 08:30:41 -0400

    Fix up some importing errors from Thimble

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+var currentUser = null;
+
+function displayLists (listsObj) {
+    if (currentUser != null) {
+        
+    } else {
+        $ (".your-lists .not-signed-in.msg").css ("opacity", "0.38");
+    }
+}
+
 $ (document).ready (function () {
     var xhr;
     var yourListsLoaded = false;
@@ -10,12 +20,11 @@ $ (document).ready (function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
             $ (".your-lists .spinner-container").css ("opacity", "0");
             yourListsLoaded = true;
-            console.log ("Your Lists XHR returned successfully");
+            displayLists ();
         }
         if (xhr.readyState == 4 && xhr.status != 200) {
             $ (".your-lists .spinner-container").css ("opacity", "0");
             $ (".your-lists .could-not-load.msg").css ("opacity", "0.38");
-            console.log ("XHR pointed at " + xhr. + " returned with error " + xhr.status);
         }
     }
     xhr.open ("GET", "../data/lists.json?requestid=" + Math.random (), true);
